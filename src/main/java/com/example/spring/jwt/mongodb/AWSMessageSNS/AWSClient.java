2c18c1eef0a90502a2f8f84924a5be69afd6cd10
shitalpbhinge <sbhinge@inferyx.com>
2023-04-27 16:15:59 +0530

    AWSSendingMesssage

--- src/main/java/com/example/spring/jwt/mongodb/AWSMessageSNS/AWSClient.java
+++ src/main/java/com/example/spring/jwt/mongodb/AWSMessageSNS/AWSClient.java
@@ -18,36 +18,104 @@ import com.amazonaws.services.sns.model.MessageAttributeValue;
 @Service
 public class AWSClient 
 {
-    public static final String accessKey = "AKIA2W4GCXAW4VJC67FN";
-    public static final String secretKey = "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";
-    
-    public void sendSingleSMS(String message, String phoneNumber) {
-   // AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.AP_SOUTH_1).build();
-    AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion("ap-south-1")
-            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
-        Map<String, MessageAttributeValue> smsAttribute = new HashMap<>();
-    smsAttribute.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue().withStringValue("myWebsite").withDataType("String"));
-    smsAttribute.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue().withStringValue("Transactional").withDataType("String"));
-    
-    PublishResult result = snsClient.publish(new PublishRequest()
-                                    .withMessage(message)
-                                    .withPhoneNumber(phoneNumber)
-                                    .withMessageAttributes(smsAttribute));
-    }
-    public void sendMultipleSMS(String message, List<String> phoneNumbers) {
-        // AmazonSNS snsClient = AmazonSNSClient.builder().withRegion(Regions.AP_SOUTH_1).build();
-        AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion("ap-south-1")
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
-        Map<String, MessageAttributeValue> smsAttribute = new HashMap<>();
-        smsAttribute.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue().withStringValue("myWebsite").withDataType("String"));
-        smsAttribute.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue().withStringValue("Transactional").withDataType("String"));
-        
-        for (String phoneNumber : phoneNumbers) {
-            PublishResult result = snsClient.publish(new PublishRequest()
-                                        .withMessage(message)
-                                        .withPhoneNumber(phoneNumber)
-                                        .withMessageAttributes(smsAttribute));
-        }
-    }
+	 public static final String accessKey = "AKIA2W4GCXAW4VJC67FN";
+	    public static final String secretKey = "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";
+	    public static final String arn = "arn:aws:sns:ap-south-1:736331216941:iscolto";
+	    public void sendSingleSMS(String message, String phoneNumber) 
+	    {
+			  
+			    AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion("ap-south-1")
+			            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
+			        
+			    Map<String, MessageAttributeValue> smsAttribute = new HashMap<>();
+			 //   smsAttribute.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue().withStringValue("myWebsite").withDataType("String"));
+			    smsAttribute.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue().withStringValue("Transactional").withDataType("String"));
+			    
+			    PublishResult result = snsClient.publish(new PublishRequest()
+			                                    .withMessage(message)
+			                                    .withPhoneNumber(phoneNumber)
+			                                    .withMessageAttributes(smsAttribute));
+	    }
+
+		public void sendMultipleSMS(String message, String[] phoneNumbers) {
+		
+		    if (message == null || message.isEmpty()) {
+		        throw new IllegalArgumentException("Message cannot be null or empty");
+		    }
+		
+		    if (phoneNumbers == null || phoneNumbers.length == 0) {
+		        throw new IllegalArgumentException("Phone numbers cannot be null or empty");
+		    }
+		
+		    AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion("ap-south-1")
+		            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
+		      
+		
+		    Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
+		    smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+		            .withStringValue("Transactional")
+		            .withDataType("String"));
+		
+		    PublishRequest request = new PublishRequest()
+		            .withMessage(message)
+		            .withMessageAttributes(smsAttributes);
+		
+		    for (String phoneNumber : phoneNumbers) {
+		        if (phoneNumber == null || phoneNumber.isEmpty()) {
+		            System.err.println("Skipping null or empty phone number");
+		            continue;
+		        }
+		        request.setPhoneNumber(phoneNumber);
+		
+		        try {
+		            PublishResult result = snsClient.publish(request);
+		            System.out.println("SMS sent to " + phoneNumber + ". Message ID: " + result.getMessageId());
+		        } catch (Exception e) {
+		            System.err.println("Error occurred while sending the SMS message to " + phoneNumber);
+		            e.printStackTrace();
+		        } 
+		        }
+		    }
+		
+		/*public void sendMultipleSMSOTP(String[] phoneNumbers) {
+		    
+		    String verificationCode = generateVerificationCode(); // Generate a random verification code
+		    
+		    String message = "Your verification code is: " + verificationCode; // Create the message string with the verification code
+		    
+		    AmazonSNS snsClient = AmazonSNSClientBuilder.standard().withRegion("ap-south-1")
+		            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))).build();
+		      
+		    Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
+		    smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+		            .withStringValue("Transactional")
+		            .withDataType("String"));
+		    
+		    PublishRequest request = new PublishRequest()
+		            .withMessage(message)
+		            .withMessageAttributes(smsAttributes);
+		    
+		    for (String phoneNumber : phoneNumbers) {
+		        if (phoneNumber == null || phoneNumber.isEmpty()) {
+		            System.err.println("Skipping null or empty phone number");
+		            continue;
+		        }
+		        request.setPhoneNumber(phoneNumber);
+		    
+		        try {
+		            PublishResult result = snsClient.publish(request);
+		            System.out.println("SMS sent to " + phoneNumber + ". Message ID: " + result.getMessageId());
+		        } catch (Exception e) {
+		            System.err.println("Error occurred while sending the SMS message to " + phoneNumber);
+		            e.printStackTrace();
+		        } 
+		    }
+		}
+
+		private String generateVerificationCode() {
+		    // Generate a random 6-digit verification code
+		    int code = (int) (Math.random() * 900000) + 100000;
+		    return String.valueOf(code);
+		}*/
 
 }
--- src/main/java/com/example/spring/jwt/mongodb/AWSMessageSNS/AWSController.java
+++ src/main/java/com/example/spring/jwt/mongodb/AWSMessageSNS/AWSController.java
@@ -12,39 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/sms")
 public class AWSController 
 {
-    @Autowired
-    private AWSClient awsSNSClient;
-    @GetMapping("/send")
-    public ResponseEntity<String> sendMessage(@RequestParam("message")String message,@RequestParam("phoneNumber")String phoneNumber){
-    try
-    {
-            awsSNSClient.sendSingleSMS(message,phoneNumber);
-            return ResponseEntity.ok("Message Sent successfully");
-    }
-    catch(Exception e)
-    {
-            e.getStackTrace();
-            e.toString();
-            return (ResponseEntity<String>) ResponseEntity.internalServerError();
-        }
-    }
-    /*@GetMapping("/sms/send")
-    public String sendSMS(@RequestParam("message") String message, @RequestParam("phoneNumber") String phoneNumber) {
-        // Call the sendSingleSMS method with the message and phoneNumber parameters
-        awsSNSClient.sendSingleSMS(message, phoneNumber);
-        return "SMS sent successfully";
-    }
-    @GetMapping("/send-multiple-sms")
-    public ResponseEntity<String> sendMultipleSMS() {
-        String message = "Hello from my website!";
-        List<String> phoneNumbers = Arrays.asList("7387403654", "9122415905", "5551234567");
-        
-        try {
-        	 awsSNSClient. sendMultipleSMS(message, phoneNumbers);
-            return ResponseEntity.ok("SMS messages sent successfully!");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending SMS messages: " + e.getMessage());
-        }
-    }*/
+	 @Autowired
+	    private AWSClient awsSNSClient;
+	    @GetMapping("/send")
+	    public ResponseEntity<String> sendMessage(@RequestParam("message")String message,@RequestParam("phoneNumber")String phoneNumber){
+	    try
+	    {
+	            awsSNSClient.sendSingleSMS(message,phoneNumber);
+	            return ResponseEntity.ok("Message Sent successfully");
+	    }
+	    catch(Exception e)
+	    {
+	            e.getStackTrace();
+	            e.toString();
+	            return (ResponseEntity<String>) ResponseEntity.internalServerError();
+	        }
+	    }
+	    
+	    @GetMapping("/sendmultiple")
+	    public ResponseEntity<String> sendMessage(@RequestParam("message")String message,@RequestParam("phoneNumber")String[] phoneNumber){
+	    try
+	    {
+	            awsSNSClient.sendMultipleSMS(message,phoneNumber);
+	            return ResponseEntity.ok("Message Sent successfully");
+	    }
+	    catch(Exception e)
+	    {
+	            e.getStackTrace();
+	            e.toString();
+	            return (ResponseEntity<String>) ResponseEntity.internalServerError();
+	        }
+	    }
 
 }
